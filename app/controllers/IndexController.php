8451dea45f6ee6b4be573621dc9f7e68efaf47f5
candy-paper <1084790021@qq.com>
2015-08-16 13:17:28 +0800

    打印server 变量

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -7,6 +7,8 @@ class IndexController extends Controller
     public function indexAction()
     {
         echo "<h1>Hello!</h1>";
-        echo $_SERVER['DOCUMENT_URI'];
+        foreach($_SERVER as $k => $v){
+            echo $k . "=" . $v . "<br/>";
+        }
     }
 }
