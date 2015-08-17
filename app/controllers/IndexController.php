3439144f682be8914ffc466686708456a4168579
huhai463127310 <huhai463127310@126.com>
2015-08-17 22:17:58 +0800

    add query by uri

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -6,13 +6,24 @@ class IndexController extends Controller
 
     public function indexAction()
     {
-        echo "<h1>Hello!</h1>";
-
-        $rules = Rule::find();
+        //echo "<h1>Hello!</h1>";
+        //$params = array_merge($_GET, $_POST);
+        $url=$_SERVER['DOCUMENT_URI'];
+        $rules = Rule::find(array(
+            array(
+                'url'=> $url,
+            )
+        ));
         if($rules){
             foreach($rules as $rule){
-                echo sprintf('id=%s url=%s', $rule->_id, $rule->url ) . "<br/>";
+                if(is_array($rule->res) || is_object($rule->res)){
+                    echo json_encode($rule->res);
+                }else{
+                    echo $rule->res;
+                }
             }
+        }else{
+            echo '{"error":1, "msg":"can\'t match any url"}';
         }
     }
 }
